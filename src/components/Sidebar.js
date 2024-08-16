34656844d7edecd3f3a58ad851822245cf4b8f04
asearer <amsearer84@gmail.com>
2024-08-16 16:56:24 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   src/components/Sidebar.css 	modified:   src/components/Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import './Sidebar.css'; // Ensure this is correctly imported
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <Nav className="flex-column">
-        <Nav.Link as={Link} to="/dashboard" className="nav-item">
-          <FaHome className="nav-icon" /> Dashboard
-        </Nav.Link>
-        <Nav.Link as={Link} to="/messages" className="nav-item">
-          <FaUser className="nav-icon" /> Messages
-        </Nav.Link>
-        <Nav.Link as={Link} to="/tasks" className="nav-item">
-          <FaCog className="nav-icon" /> Tasks
-        </Nav.Link>
-        <Nav.Link as={Link} to="/profile" className="nav-item">
-          <FaUser className="nav-icon" /> Profile
-        </Nav.Link>
-        
-      </Nav>
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <Nav className="flex-column">
+          <Nav.Link as={Link} to="/dashboard" className="nav-item">
+            <FaHome className="nav-icon" /> Dashboard
+          </Nav.Link>
+          <Nav.Link as={Link} to="/messages" className="nav-item">
+            <FaUser className="nav-icon" /> Messages
+          </Nav.Link>
+          <Nav.Link as={Link} to="/tasks" className="nav-item">
+            <FaCog className="nav-icon" /> Tasks
+          </Nav.Link>
+          <Nav.Link as={Link} to="/profile" className="nav-item">
+            <FaUser className="nav-icon" /> Profile
+          </Nav.Link>
+        </Nav>
+      </div>
     </div>
   );
 };
 
 export default Sidebar;
+
 
 
